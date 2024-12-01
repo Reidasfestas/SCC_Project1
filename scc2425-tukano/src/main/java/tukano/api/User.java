c4e65e3f78ada5298a32193de5a381b19fd1add5
Frederico <fmg.silva@fct.unl.pt>
2024-12-01 00:55:59 +0000

    conection working

--- scc2425-tukano/src/main/java/tukano/api/User.java
+++ scc2425-tukano/src/main/java/tukano/api/User.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Users")
 public class User {
 	@Id
 	@JsonProperty("id")
